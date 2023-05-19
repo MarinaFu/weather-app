8da4a2384bc27faea08d007090ad9007d3a82243
MarinaFu <133999176+MarinaFu@users.noreply.github.com>
2023-05-19 11:23:31 +0200

    Created JS

---
+++ src/index.js
@@ -0,0 +1,92 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let currentYear = now.getFullYear();
+let currentDay = days[now.getDay()];
+let currentMonth = months[now.getMonth()];
+let currentHour = now.getHours();
+if (currentHour < 10) {
+  currentHour = `0${currentHour}`;
+}
+let currentMinute = now.getMinutes();
+if (currentMinute < 10) {
+  currentMinute = `0${currentMinute}`;
+}
+let currentDate = now.getDate();
+
+let h3 = document.querySelector("h3");
+
+h3.innerHTML = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}, ${currentHour}:${currentMinute}`;
+
+function search(event) {
+  event.preventDefault();
+  let input = document.querySelector("#inputSearch");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = input.value;
+}
+let form = document.querySelector(".searching-engine");
+form.addEventListener("submit", search);
+
+// 17/05/2023
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#searchCity").innerHTML = response.data.name;
+  let currentTemperature = document.querySelector("#current-temp");
+  currentTemperature.innerHTML = `${temperature} ° C`;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchTemp(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#inputSearch").value;
+  let apiKey = "9fc1ce91583db8398ec357be4554346e";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let searchForm = document.querySelector(".searching-engine");
+searchForm.addEventListener("submit", searchTemp);
+
+function showPosition(position) {
+  let apiKey = "9fc1ce91583db8398ec357be4554346e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
